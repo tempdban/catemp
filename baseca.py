a0940255a81857ba8619c833adbd5cf0c748ed9a
tempdban <temp@dban.tk>
2015-05-04 04:19:45 +0800

    init

---
+++ baseca.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+class baseca(object):
+	"""docstring for baseca"""
+	def __init__(self, arg1,arg2):
+		self.__m = arg1
+		self.__n = arg2
+		self.__arr1 = {}
+		self.__arr2 = {}
+		for i in __m:
+			for j in __n:
+				__arr1[(__m,__n)] = 0
+				__arr2[(__m,__n)] = 0
+	def __checkmn(self, arg1,arg2):
+		
+		
